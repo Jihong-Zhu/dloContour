298e88914d7dfd21daaa293ae59b120f124018a2
Jihong Zhu <zhu@lirmm.fr>
2018-05-30 11:55:48 +0200

    A robust contour detection alggorithm

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,60 @@ using namespace std;
 int thresh = 100;
 Mat canny_output;
 vector<vector<Point> > contours;
+
+struct ImageROI
+{
+  int x_min;
+  int x_max;
+  int y_min;
+  int y_max;
+};
+
+enum SegmentationType
+{
+  Black,
+  Red,
+};
+
+ImageROI roi = {350, 1100, 400, 1460};
 vector<Vec4i> hierarchy;
-RNG rng(12345); // for contour drawing
+
+cv::Mat imageSegmentation(const cv::Mat& color_image, SegmentationType type, ImageROI roi, int theshold = 50) {
+	Rect2i ROI{roi.x_min, roi.y_min, roi.x_max - roi.x_min, roi.y_max - roi.y_min};            // create a rectangle for selecting ROI (x_min, y_min, x_max-x_min, y_max-y_min)
+	Mat imageROI = color_image(ROI);
+	Mat binary_image;                            // Binary image
+	switch(type) {
+	case SegmentationType::Black:
+	{
+		Mat imageGray;                              // Gray scale image
+		cv::cvtColor(imageROI, imageGray, CV_BGR2GRAY);
+		const int maxValue = 255;                         // Maximum value
+		cv::threshold(imageGray, binary_image, theshold, maxValue, THRESH_BINARY);      // Binary image
+	}
+	break;
+	case SegmentationType::Red:
+	{
+		cv::Mat hsv_img;
+		cv::cvtColor(imageROI, hsv_img, cv::COLOR_BGR2HSV);
+
+		// Threshold the HSV image, keep only the red pixels
+		cv::Mat mask1;
+		cv::Mat mask2;
+		cv::inRange(hsv_img, cv::Scalar(0, 100, 100), cv::Scalar(theshold, 255, 255), mask1);
+		cv::inRange(hsv_img, cv::Scalar(179-theshold, 100, 100), cv::Scalar(179, 255, 255), mask2);
+		cv::Mat detectRed = mask1 | mask2;        // With this red will be white and rest will be black
+		// Reverse black and white to fit with the active contour alg.
+		cv::bitwise_not(detectRed, binary_image);
+	}
+	break;
+	}
+
+	Mat filtered_image;
+	cv::erode(binary_image, filtered_image, Mat());
+	cv::dilate(binary_image, filtered_image, Mat());
+
+	return filtered_image;
+}
 
 int main(int argc, char const *argv[]) {
   if( argc != 2)
@@ -24,10 +76,15 @@ int main(int argc, char const *argv[]) {
     }
   Mat image;
   image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-  Canny( image, canny_output, thresh, thresh*2, 3 );
+  std::cout << "size of the image: " << image.rows << " " << image.cols << '\n';
+  SegmentationType type(Black);
+  Mat segmentedImage = imageSegmentation(image, type, roi);
+  Canny( segmentedImage, canny_output, thresh, thresh*2, 3 );
   findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+  waitKey(0);
   Mat contourData(contours[0][0]);  // create a mat to store the contour
   int index = 0;
+  std::cout << "contour size " << contours.size() << '\n';
   // Find the contour with most points(it must be the right contour)
   for(int i= 0; i < contours.size(); i++)
   {
